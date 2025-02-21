63e31802394ba0c9470432a252622429a70e2dd3
ZackWmechanical <zackw123321@gmail.com>
2025-02-21 15:26:05 -0500

    Calibrating Errors with sensor (Current problem)

--- AirbrakesAltimeterTest/src/main.cpp
+++ AirbrakesAltimeterTest/src/main.cpp
@@ -1,18 +1,79 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    if (!bno.begin()) {
+        Serial.println("No BNO055 detected, check wiring or I2C ADDR!");
+        while (1); // Stop execution if the sensor is not detected
+    }
+    delay(1000);
+    
+    bno.setExtCrystalUse(true); // Use external crystal for better accuracy
+
+    Serial.println("Setting operation mode to NDOF...");
+    bno.setMode(0x0C);
+
+    // Commented properly to avoid syntax errors
+    /*
+    Serial.println("Place the sensor in 6 different stable positions for calibration.");
+    while (!bno.isFullyCalibrated()) {
+        delay(500);
+        uint8_t system, gyro, accel, mag;
+        system = gyro = accel = mag = 0;
+        bno.getCalibration(&system, &gyro, &accel, &mag);
+        Serial.print("CALIBRATION: Sys=");
+        Serial.print(system);
+        Serial.print(" Gyro=");
+        Serial.print(gyro);
+        Serial.print(" Accel=");
+        Serial.print(accel);
+        Serial.print(" Mag=");
+        Serial.println(mag);
+    }
+    */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Check calibration status
+    uint8_t system, gyro, accel, mag;
+    system = gyro = accel = mag = 0;
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    
+    Serial.print("CALIBRATION: Sys=");
+    Serial.print(system);
+    Serial.print(" Gyro=");
+    Serial.print(gyro);
+    Serial.print(" Accel=");
+    Serial.print(accel);
+    Serial.print(" Mag=");
+    Serial.println(mag);
+
+    // Check sensor status
+    uint8_t system_status, self_test_result, system_error;
+    bno.getSystemStatus(&system_status, &self_test_result, &system_error);
+
+    Serial.print("System Status: ");
+    Serial.println(system_status);
+    Serial.print("Self Test Result: ");
+    Serial.println(self_test_result);  // 0x0F means all good
+    Serial.print("System Error: ");
+    Serial.println(system_error);
+
+    // Get Euler angles
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.println(euler.z());
+
+    delay(100);
 }
